6fffe84a06908149d683ebaaf647c1b3b05a5e87
pbuzak <16011@my.sanctamaria.school.nz>
2024-04-17 21:39:02 +1200

    Challenge 2 Done

--- day18/day18_proj.py
+++ day18/day18_proj.py
@@ -4,9 +4,17 @@ timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("red")
 
-for _ in range(4):
-    timmy.right(90)
-    timmy.forward(100)
+# for _ in range(4):
+#     timmy.right(90)
+#     timmy.forward(100)
+
+for _ in range(10):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pd()
+
 
 screen = Screen()
 screen.exitonclick()
+
