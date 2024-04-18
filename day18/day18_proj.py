5ba9e145b6ca809dcf1e2e563451580ecab35ddb
pbuzak <16011@my.sanctamaria.school.nz>
2024-04-18 21:41:28 +1200

    Start of Project

--- day18/day18_proj.py
+++ day18/day18_proj.py
@@ -31,13 +31,13 @@ timmy.color("red")
 timmy.speed(0)
 timmy.pensize(1)
 
-# def pick_direction():
-#     direction = [0, 90, 180, 270]
-#     return direction[random.randint(0, 3)]
+def pick_direction():
+    direction = [0, 90, 180, 270]
+    return direction[random.randint(0, 3)]
 
-# def pick_color():
-#     color_tuple = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-#     return timmy.pencolor(color_tuple)
+def pick_color():
+    color_tuple = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    return timmy.pencolor(color_tuple)
 
 # for _ in range(50):
 #     direction = pick_direction()
@@ -47,9 +47,28 @@ timmy.pensize(1)
 
 
 
-for _ in range(100):
-    timmy.circle(200)
-    timmy.tilt(20)
+### my take on Challenge 5
+# angle = 0
+# for _ in range(75):
+#     if angle == 360:
+#         break   
+#     pick_color()
+#     timmy.circle(100)
+#     timmy.seth(angle)
+#     angle += 5
+
+
+### answer:
+def draw(gap):
+    for _ in range (int(360/gap)):
+        pick_color()
+        timmy.circle(100)
+        timmy.seth(timmy.heading() + gap)
+
+
+
+
+    
 
 screen = Screen()
 screen.exitonclick()
