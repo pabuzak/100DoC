747c42a14c0847b33e7b5f454b4b8f622e5ecec3
pabuzak <pabuzak@gmail.com>
2024-04-23 21:11:17 +1200

    End of Day 20

--- day 20/day20_proj.py
+++ day 20/day20_proj.py
@@ -10,6 +10,12 @@ s.tracer(0)
 
 snake = Snake()
 
+s.listen()
+s.onkey(key="Up", fun=snake.up)
+s.onkey(key="Down", fun=snake.down)
+s.onkey(key="Left", fun=snake.left)
+s.onkey(key="Right", fun=snake.right)
+
 game_is_on = True
 while game_is_on:
     s.update()
--- day 20/snake.py
+++ day 20/snake.py
@@ -1,9 +1,14 @@
 from turtle import Turtle
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.snakes = []
         self.create_snake()
+        self.head = self.snakes[0]
 
     def create_snake(self):
         for i in range(3):
@@ -20,4 +25,19 @@ class Snake:
             self.snakes[seg_num].goto(new_x, new_y)
         self.snakes[0].forward(20)
 
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
 
