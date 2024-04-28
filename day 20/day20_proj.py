56cc3f2d074f502b0bcc6fbb7f78d388605fb3de
pabuzak <pabuzak@gmail.com>
2024-04-28 22:38:31 +1200

    Day 21 Completed

--- day 20/day20_proj.py
+++ day 20/day20_proj.py
@@ -38,10 +38,8 @@ while game_is_on:
         scoreboard.game_over()
 
     #Detect collision with tail.
-    for segment in snake.snakes:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.snakes[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
