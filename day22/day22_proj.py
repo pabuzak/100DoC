a864455c1addc156ae9e5dd65114840cd0c3ad12
pabuzak <pabuzak@gmail.com>
2024-05-05 00:21:48 +1200

    Day 22 First Challenge Completed

--- day22/day22_proj.py
+++ day22/day22_proj.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import time
 
 s = Screen()
 s.setup(width=800, height=600)
@@ -13,7 +14,17 @@ rpaddle.up()
 rpaddle.goto(x=350, y=0)
 rpaddle.shapesize(stretch_len=1, stretch_wid=5)
 
+def up():
+    x, y = rpaddle.pos()
+    rpaddle.goto(y=y-20, x=x)
+
+def down():
+    x, y = rpaddle.pos()
+    rpaddle.goto(y=y+20, x=x)
+
 s.listen()
+s.onkey(key="Up", fun=down)
+s.onkey(key="Down", fun=up)
 
 
 
